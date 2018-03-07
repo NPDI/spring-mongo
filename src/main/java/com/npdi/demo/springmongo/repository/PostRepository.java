09f4994466faee6ee1b8d441574d5d9d3b85fd5d
Diego Dulval <diegodifreitas@gmail.com>
2018-03-07 09:52:02 -0300

    Added @Query in PostRepository

--- src/main/java/com/npdi/demo/springmongo/repository/PostRepository.java
+++ src/main/java/com/npdi/demo/springmongo/repository/PostRepository.java
@@ -8,10 +8,14 @@ package com.npdi.demo.springmongo.repository;
 import com.npdi.demo.springmongo.domain.Post;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
     public List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex : ?0, $options: 'i' } }")
+    public List<Post> searchTitle(String text);
 }
